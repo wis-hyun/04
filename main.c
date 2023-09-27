f7976e109ff86cf7b35a08c8032479a964b2ed1a
김성현 <khyun8576@naver.com>
2023-09-27 14:02:33 +0900

    3

--- main.c
+++ main.c
@@ -4,15 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x, y;
-	printf("input two integers :");
-	scanf("%i %i", &x, &y);
+	int x;
 	
-	printf("+ result is %i\n", x+y);
-	printf("- result is %i\n", x-y);
-	printf("* result is %i\n", x*y);
-	printf("/ result is %i\n", x/y);
-	printf("%% result is %i\n", x%y);
+	printf("input the second :");
+	scanf("%i", &x);
+	
+	printf("the time is %i : %i", x/60, x%60);
 	
 	return 0;
 }
