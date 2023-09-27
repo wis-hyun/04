353269510a973e00a2d42f1104a959ff214a97b7
김성현 <khyun8576@naver.com>
2023-09-27 14:26:48 +0900

    4

--- main.c
+++ main.c
@@ -5,11 +5,22 @@
 
 int main(int argc, char *argv[]) {
 	int x;
+	int y;
 	
-	printf("input the second :");
+	printf("input the year :");
 	scanf("%i", &x);
 	
-	printf("the time is %i : %i", x/60, x%60);
+	if ((x%4==0 && x%100!=0)||(x%400==0))
+	{
+		y=1;
+	}
+
+	else
+	{
+		y=0;
+	}
+		
+	printf("is the year %i the leap year? : %i", x, y);
 	
 	return 0;
 }
